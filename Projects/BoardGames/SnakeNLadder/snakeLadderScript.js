6786a9ca911472eed14691384177cc9faa09129d
Tejas Jadhav <tjadhav9@gmail.com>
2020-11-22 15:46:50 +0530

    jpg FIX

--- Projects/BoardGames/SnakeNLadder/snakeLadderScript.js
+++ Projects/BoardGames/SnakeNLadder/snakeLadderScript.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
 	var boardImgNmbr = 0;
-	var boardImgPath = "images/board" + boardImgNmbr +"";
+	var boardImgPath = "images/board" + boardImgNmbr +".jpg";
 	$(".board").attr("src",boardImgPath);
 	
 	$("#startcontrols").css("display","");
@@ -207,7 +207,7 @@ $(document).ready(function()
 		$(".boardSelected").removeClass("boardSelected");
 		$(this).addClass("boardSelected");
 		boardImgNmbr = parseInt(this.id.split("_")[1]);
-		boardImgPath = "images/board" + boardImgNmbr +"";
+		boardImgPath = "images/board" + boardImgNmbr +".jpg";
 		$(".board").attr("src",boardImgPath);
 	});
 
